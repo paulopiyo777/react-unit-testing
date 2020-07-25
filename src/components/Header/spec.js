f7b5539bc33beaae4bf5eca8f38aeb97f754e666
Paul Opiyo <59094545+paulopiyo777@users.noreply.github.com>
2020-07-25 13:01:20 -0500

    add jest dependencies

---
+++ src/components/Header/spec.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Header from "./index";
+
+// testthat checks whether functional component renders correctly
+it("It should render without errors", () => {
+  expect(1).toBe(1);
+});
